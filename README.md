60e910d162caf4fee4de6f46c1f47282e618fb46
huqa <pikkuhukka@gmail.com>
2014-10-13 22:57:23 +0300

    Service providers for forms, twig and db abstraction layer. Some boilerplate too.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ swaeg-regger
 Party-goers registering application
 
 ## First time installation
-	run
-	php composer.phar install
+	run: php composer.phar install
 
 noice _b
--- web/index.php
+++ web/index.php
@@ -1,11 +1,44 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Namespaces
+use Silex\Provider\FormServiceProvider;
+
+
 $app = new Silex\Application();
 
-$app->get('/juuh', function () use ($app) {
-    return 'Juuh elikkäs';
+// Debug mode
+$app['debug'] = true;
+
+// Register Twig service provider
+$app->register(new Silex\Provider\TwigServiceProvider(), 
+		array('twig.path' => __DIR__.'/../views',
+));
+// Register Doctrine db abstraction layer
+$app->register(new Silex\Provider\DoctrineServiceProvider(), 
+		array('db.options' => array(
+				'driver'   => 'pdo_sqlite',
+				'path'     => __DIR__.'/../db/app.db')
+));
+
+$app->register(new FormServiceProvider());
+
+// This route handles the data from the form
+$app->post('/tuuppaa', function (Request $request) use($app) {
+
 });
+
+// This route is the simple main route, just renders the form
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('main.twig');
+});
+
+
+/*$app->get('/init-db', function() use($app) {
+	$sql = "CREATE TABLE IF NOT EXISTS attendees(id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, email TEXT NOT NULL)";
+	$app['db']->executeQuery($sql);
+	return "Init db!";
+});*/
 
 $app->run();
 
