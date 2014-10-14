cef978500a376bc33c36106685b93afecee3e0d5
huqa <pikkuhukka@gmail.com>
2014-10-14 16:07:13 +0300

    Reset console command working

---
+++ commands/ResetCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+use Knp\Command\Command;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+
+class ResetCommand extends Knp\Command\Command {
+	
+	protected function configure() {
+		$this->setName('reset')->setDescription("Resets the application");
+	}
+	
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$helper = $this->getHelper('question');
+		$question = new ConfirmationQuestion('Are you 100% sure you want to reset all data? (y/n) ', false);
+		if (!$helper->ask($input, $output, $question)) {
+	    		return;
+		}
+		$app = $this->getSilexApplication();
+		$deleteSql = "DROP TABLE IF EXISTS attendees";
+		$app['db']->executeQuery($deleteSql);
+		$output->writeln('Old database dropped.');
+		$createSql = "CREATE TABLE IF NOT EXISTS attendees(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL)";
+		$app['db']->executeQuery($createSql);
+		$output->writeln('New database created.');
+		$output->writeln('== DONE ==');
+	}	
+}
